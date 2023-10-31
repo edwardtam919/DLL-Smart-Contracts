0cd040071509104914f95fe86fc142af678d24ab
Edward <edward.tam@zynersoft.com>
2023-10-31 16:06:09 +0800

    - updated 20231031-2 based on Certik's comments

--- contracts/MintPass.sol
+++ contracts/MintPass.sol
@@ -7,32 +7,45 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/token/common/ERC2981.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol"; // IERC20 interface
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract MintPassNFT is ERC721, ERC721URIStorage, Pausable, Ownable, ERC721Burnable, ERC2981 {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIdCounter;
+contract MintPassNFT is ERC721, ERC721URIStorage, Pausable, Ownable, ERC721Burnable, ERC2981, ReentrancyGuard {
+
     using ECDSA for bytes32;
-    using SafeMath for uint256;
+    using Address for address payable;
 
-    address private systemAddress;
-    address private mintingFeeRecipient;
+    address private immutable systemAddress;
+    address private immutable mintingFeeRecipient1;
+    address private immutable mintingFeeRecipient2;
+    address private immutable mintingFeeRecipient3;
     uint256 private mintingFee;
-    uint96 private loyaltyFee;
-    address contractOwner;
+    uint96 private immutable loyaltyFee;
+    address immutable contractOwner;
+    uint256 private _tokenIdCounter;
+    uint private lastMintingFeeChangeTime;
 
+    // Mapping of addresses who has been used
+    mapping(bytes32 => bool) public hashUsed;
+
+    // timelock
+    uint public constant lockDuration = 48 hours;
+
+    // events
     event tokenIdMinted(uint256 indexed tokenId);
+    event mintingFeeSet(uint256 indexed mintingFee);
 
-    // constructor takes the systemAddress (for signature verification), minting recipient, base URI, minting fee & loyalty fee
-    constructor(address _systemAddress, address _mintingFeeRecipient, uint256 _mintingFee,  uint96 _loyaltyFee) ERC721("MintPass", "MPASS") {
+    // constructor takes the systemAddress (for signature verification), minting fee recipients, base URI, minting fee & loyalty fee
+    constructor(address _systemAddress, address _mintingFeeRecipient1, address _mintingFeeRecipient2, address _mintingFeeRecipient3 , uint256 _mintingFee,  uint96 _loyaltyFee) ERC721("Mindful Ocean Mint Pass", "MPASS") {
         contractOwner = msg.sender;
         systemAddress = _systemAddress;
-        mintingFeeRecipient = _mintingFeeRecipient;
+        mintingFeeRecipient1 = _mintingFeeRecipient1;
+        mintingFeeRecipient2 = _mintingFeeRecipient2;
+        mintingFeeRecipient3 = _mintingFeeRecipient3;
         mintingFee = _mintingFee;
         loyaltyFee = _loyaltyFee;
+        lastMintingFeeChangeTime = block.timestamp; 
     }
 
     // recover Signer from hash & signature
@@ -53,8 +66,17 @@ contract MintPassNFT is ERC721, ERC721URIStorage, Pausable, Ownable, ERC721Burna
 
     // set minting fee, only contract creator can call
     function setMintingFee(uint256 _mintingFee) public  {
-        require(contractOwner == msg.sender);
+
+        // check if contract Owner
+        require(contractOwner == msg.sender, "Must be Contract Owner to call the function");
+
+        // check if pass lock duration
+        uint endLocking = lastMintingFeeChangeTime + lockDuration;
+        require(block.timestamp >= endLocking, "Need to wait 2 minutes before change minting fee again");
+
         mintingFee = _mintingFee;
+        lastMintingFeeChangeTime = block.timestamp;
+        emit mintingFeeSet(mintingFee);
     }
 
     // get minting fee
@@ -62,20 +84,38 @@ contract MintPassNFT is ERC721, ERC721URIStorage, Pausable, Ownable, ERC721Burna
         return mintingFee;
     }
 
+    // get lastMintingFeeChangeTime
+    function getLastMintingFeeChangeTime() public view returns (uint) {
+        return lastMintingFeeChangeTime;
+    }
+
     // mint Metaverse NFT
-    function mintMintPass(bytes32 hash, bytes memory signature, string memory uri) public payable returns(uint256){
+    function mintMintPass(bytes32 hash, bytes calldata signature, string calldata uri) public payable whenNotPaused nonReentrant returns(uint256){
+
+        // check if hash has been used
+        require(!hashUsed[hash], "Hash has been used");
+
+        // set the hash as used
+        hashUsed[hash] = true;
 
         // check signature
         require(recoverSigner(hash, signature) == systemAddress, "Signature Failed");
 
         // transfer minting fee to the defined wallet
         require(msg.value >= mintingFee, "Not enough MATIC sent; check price!"); 
-        payable(address(mintingFeeRecipient)).transfer(mintingFee);
+        payable(address(mintingFeeRecipient1)).sendValue(mintingFee * 60 / 100);
+        payable(address(mintingFeeRecipient2)).sendValue(mintingFee * 15 / 100);
+        payable(address(mintingFeeRecipient3)).sendValue(mintingFee * 25 / 100);
+
+        //refund any excess native tokens sent by the user
+        if (msg.value > mintingFee){
+            payable(address(msg.sender)).sendValue(msg.value-mintingFee);
+        }
 
         // set tokeID & recipient        
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
+        uint256 tokenId = _tokenIdCounter;
         _safeMint(msg.sender, tokenId);
+        _tokenIdCounter += 1;
 
         // set loyalty fee
         _setTokenRoyalty(tokenId, msg.sender, loyaltyFee);
